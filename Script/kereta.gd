ccfe09b0ecdc47c18586fde00148f6a27f22d48b
GanisAD <ganis.ahmad.com@gmail.com>
2026-06-18 15:03:39 +0700

    Added Gerbong Physic and more "Make Sense" jumping

--- Script/kereta.gd
+++ Script/kereta.gd
@@ -1,52 +1,44 @@
-extends CharacterBody2D
+extends Node2D
 
-# --- Konfigurasi Pergerakan ---
-@export var SPEED = 300.0
-@export var JUMP_VELOCITY = -500.0
+@onready var kepala_kereta = $Kepala/CharacterBody2D
+@onready var sprite_kepala = $Kepala/CharacterBody2D/Sprite2D
+@onready var kumpulan_gerbong = $KumpulanGerbong
 
-# --- Konfigurasi Efek Tilting Parabola ---
-@export var MAX_TILT_UP = -20.0   # Derajat maksimal saat naik (hidung ke atas)
-@export var MAX_TILT_DOWN = 15.0  # Derajat maksimal saat turun (hidung menunduk)
-@export var TILT_SMOOTHNESS = 10.0 # Seberapa mulus transisi rotasinya
+@export var MAKSIMAL_GERBONG = 10 
+@export var JARAK_PIKSEL_ANTAR_GERBONG = 270 
 
-@onready var sprite = $Sprite2D
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+# Angka ini menentukan seberapa kaku/elastis sambungan keretanya.
+# - Semakin besar (misal 20.0), gerbong akan bereaksi sangat cepat (kaku).
+# - Semakin kecil (misal 5.0), gerbong akan melar dan lambat ditarik.
+@export var KEKAKUAN_RANTAI = 15.0 
 
 func _physics_process(delta):
-	# 1. Terapkan Gravitasi
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# 2. Fitur Lompat
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# 3. Gerakan Otomatis ke Depan
-	velocity.x = SPEED
-
-	# 4. Efek Tilting Parabola Berdasarkan Kecepatan Vertikal
-	var target_rotation = 0.0
-
-	if not is_on_floor():
-		# Kita jadikan rasio kecepatan saat ini dibandingkan kekuatan lompat awal
-		# Hasilnya berkisar sekitar -1.0 (naik mentok) hingga 1.0 atau lebih (jatuh cepat)
-		var fall_ratio = velocity.y / abs(JUMP_VELOCITY)
-		
-		if fall_ratio < 0:
-			# Sedang naik (velocity.y negatif)
-			target_rotation = deg_to_rad(MAX_TILT_UP) * abs(fall_ratio)
-		else:
-			# Sedang turun (velocity.y positif)
-			target_rotation = deg_to_rad(MAX_TILT_DOWN) * abs(fall_ratio)
+	var daftar_gerbong = kumpulan_gerbong.get_children()
+	
+	for i in range(daftar_gerbong.size()):
+		if i >= MAKSIMAL_GERBONG:
+			break
 			
-		# Kunci nilai rotasi agar kereta tidak berputar balik jika jatuh terlalu lama/cepat
-		target_rotation = clamp(target_rotation, deg_to_rad(MAX_TILT_UP), deg_to_rad(MAX_TILT_DOWN))
-	else:
-		# Jika menyentuh tanah, target rotasi kembali ke 0 (datar)
-		target_rotation = 0.0
-
-	# Gunakan fungsi lerp_angle untuk menggerakkan rotasi secara mulus ke targetnya
-	sprite.rotation = lerp_angle(sprite.rotation, target_rotation, TILT_SMOOTHNESS * delta)
-
-	# 5. Eksekusi pergerakan fisika
-	move_and_slide()
+		var gerbong = daftar_gerbong[i]
+		
+		# 1. KUNCI SUMBU X: Jarak horizontal dijamin tetap absolut & rapi
+		gerbong.global_position.x = kepala_kereta.global_position.x - ((i + 1) * JARAK_PIKSEL_ANTAR_GERBONG)
+		
+		# 2. TENTUKAN TARGET (Siapa yang harus diikuti oleh gerbong ini?)
+		var target_y = 0.0
+		var target_rot = 0.0
+		
+		if i == 0:
+			# Gerbong pertama selalu meniru Kepala secara langsung
+			target_y = kepala_kereta.global_position.y
+			target_rot = sprite_kepala.rotation
+		else:
+			# Gerbong ke-2 dan seterusnya meniru gerbong tepat di depannya (i - 1)
+			var gerbong_depan = daftar_gerbong[i - 1]
+			target_y = gerbong_depan.global_position.y
+			target_rot = gerbong_depan.rotation
+			
+		# 3. INTERPOLASI (Lerp): Bergerak menuju target secara berangsur-angsur
+		# Inilah yang menciptakan efek "Semi-Rigid" dan merambat dari depan ke belakang
+		gerbong.global_position.y = lerp(gerbong.global_position.y, target_y, KEKAKUAN_RANTAI * delta)
+		gerbong.rotation = lerp_angle(gerbong.rotation, target_rot, KEKAKUAN_RANTAI * delta)
