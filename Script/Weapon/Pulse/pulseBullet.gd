32220766b82465b33c2abc610a8402ff4cbece0e
GanisAD <ganis.ahmad.com@gmail.com>
2026-06-20 17:07:57 +0700

    Refined "Senjata" in kereta
    Senjata has more realistic cone vision, can shoot automatically, can target automatically, and the head sprites follow the target.

--- Script/Weapon/Pulse/pulseBullet.gd
+++ Script/Weapon/Pulse/pulseBullet.gd
@@ -6,9 +6,8 @@ extends Area2D
 
 const SPRITE_NATIVE_WIDTH = 32.0 
 
-func _ready():
-	# Karena root node (Area2D) sudah dirotasi oleh senjata, 
-	# RayCast2D otomatis akan menembak ke arah sudut yang benar!
+# UBAH NAMA FUNGSI INI DARI _ready() MENJADI fire()
+func fire():
 	ray_cast.force_raycast_update()
 	
 	var beam_length: float
@@ -16,27 +15,28 @@ func _ready():
 	if ray_cast.is_colliding():
 		var collision_point = ray_cast.get_collision_point()
 		beam_length = global_position.distance_to(collision_point)
+		
+		var musuh = ray_cast.get_collider()
+		
+		if musuh.has_method("take_damage"):
+			musuh.take_damage(10)
+		else:
+			print("Nabrak tembok/benda mati, abaikan.")
 	else:
-		# Menyesuaikan panjang maksimum sesuai target_position RayCast
 		beam_length = ray_cast.target_position.x
 
-	# Meregangkan visual (Sprite)
 	var required_scale_x = beam_length / SPRITE_NATIVE_WIDTH
 	sprite.scale.x = required_scale_x
 	
-	# Meregangkan dan memposisikan collision (Area2D)
 	var shape = collision_shape.shape.duplicate() as RectangleShape2D
 	
 	if shape:
 		shape.size.x = beam_length
 		collision_shape.position.x = beam_length / 2
-	# Terapkan shape yang sudah diduplikat dan diubah ukurannya kembali ke node
 		collision_shape.shape = shape 
 		force_update_transform()
 	else:
-		# Ini akan muncul di panel Output jika Anda salah memilih bentuk di Inspector
-		print("ERROR: Bentuk collision bukan RectangleShape2D!")	# Hancurkan laser setelah 0.1 detik
+		print("ERROR: Bentuk collision bukan RectangleShape2D!")	
+
 	await get_tree().create_timer(0.1).timeout
 	queue_free()
-
-# FUNGSI set_direction() SUDAH DIHAPUS
