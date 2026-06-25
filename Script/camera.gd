77c35a70c12e8aa8d470f6b2d20d0ff901bb99c3
GanisAD <ganis.ahmad.com@gmail.com>
2026-06-25 23:02:35 +0700

    small variable tweak
    chagens camera offsite from 700 to 1000

--- Script/camera.gd
+++ Script/camera.gd
@@ -2,7 +2,7 @@ extends Camera2D
 
 @export_group("Dynamic Offset")
 @export var default_offset_x: float = 0.0
-@export var max_lag_offset_x: float = 700.0
+@export var max_lag_offset_x: float = 1000.0
 
 @export_group("Dynamic Zoom")
 @export var default_zoom: Vector2 = Vector2(0.6, 0.6)
