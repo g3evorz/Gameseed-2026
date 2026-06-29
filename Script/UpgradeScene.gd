bd992136920e30e12f55bf3b26c7e33675c616c0
Udiin <130551799+g3evorz@users.noreply.github.com>
2026-06-29 08:38:40 +0700

    ganti kelipatan harga

--- Script/UpgradeScene.gd
+++ Script/UpgradeScene.gd
@@ -11,16 +11,12 @@ extends CanvasLayer # Atau Node2D, sesuaikan dengan tipe node 'Main' Anda
 @onready var label_lvl_defense = $MarginUpgrade/ScrollContainer/HBoxUpgrade/ItemDefense/Label
 @onready var btn_beli_defense = $MarginUpgrade/ScrollContainer/HBoxUpgrade/ItemDefense/Button
 
-
-
 # --- KONFIGURASI HARGA (Harga Dasar & Kelipatan per Level) ---
 var harga_dasar_laser = 200
-var kelipatan_harga_laser = 100 
+var kelipatan_harga_laser = 200
 
 var harga_dasar_defense = 150
-var kelipatan_harga_defense = 75
-
-
+var kelipatan_harga_defense = 100
 
 func _ready():
 	# Muat data dari save file saat menu dibuka
@@ -47,11 +43,9 @@ func update_semua_ui():
 	btn_beli_defense.disabled = ScoreManager.dompet_koin < harga_d
 	
 
-
 # Fungsi bantuan untuk menghitung harga dinamis
 func hitung_harga(dasar: int, kelipatan: int, level_saat_ini: int) -> int:
 	return dasar + (kelipatan * level_saat_ini)
-
 
 # --- FUNGSI KLIK TOMBOL BELI (Hubungkan via Inspector ke masing-masing Button) ---
 
@@ -67,12 +61,7 @@ func _on_btn_beli_defense_pressed():
 		if ScoreManager.beli_upgrade(harga, "defense"):
 			update_semua_ui()
 
-
-
-
 # --- FUNGSI NAVIGASI ---
-
-
 
 func _on_button_home_pressed():
 	get_tree().change_scene_to_file("res://Scenes/Homescreen.tscn")
