0d3aa6ff1686720c0ae5821c6cf070875415f74f
GanisAD <ganis.ahmad.com@gmail.com>
2026-07-02 14:21:20 +0700

    Fix indentation in rocket.gd

--- Script/Weapon/Musuh/rocket.gd
+++ Script/Weapon/Musuh/rocket.gd
@@ -85,7 +85,8 @@ func _on_body_entered(body: Node2D) -> void:
 		#elif body.get_parent() and body.get_parent().has_method("terima_damage"):
 			#body.get_parent().terima_damage(damage_tabrakan)
 			
-	GameManager.terapkan_efek_ram(kekuatan_slow)
+		GameManager.terapkan_efek_ram(kekuatan_slow)
+		
 	queue_free()
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
